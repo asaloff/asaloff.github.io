76ba8b722e94a03b77463bf60c5827d374de9d5a
Aaron Saloff <aaronsaloff@gmail.com>
2018-02-03 08:27:24 -0500

    load gifs after load for better UX

---
+++ js/gifLoader.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+  const djelloUrl = 'https://s3.amazonaws.com/demo-gifs-asaloff/djello_optimized.gif';
+  const fideligardUrl = 'https://s3.amazonaws.com/demo-gifs-asaloff/fideligard_optimized.gif';
+  const superchatUrl = 'https://s3.amazonaws.com/demo-gifs-asaloff/superchat.gif';
+  const mimirsMarketUrl = 'https://s3.amazonaws.com/demo-gifs-asaloff/mimirs_market_optimized.gif';
+
+  $('#djello-img').css('background-image', `url(${ djelloUrl })`);
+  $('#fideligard-img').css('background-image', `url(${ fideligardUrl })`);
+  $('#superchat-img').css('background-image', `url(${ superchatUrl })`);
+  $('#mimirs-market-img').css('background-image', `url(${ mimirsMarketUrl })`);
+});
